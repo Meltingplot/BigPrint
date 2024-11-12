7ba5ed5a1be477c5edfb9091c4b49ad13652bc19
Tim Schneider <tim@schneider.engineering>
2024-11-12 14:02:00 +0100

    override existing files

--- duetPi/install.sh
+++ duetPi/install.sh
@@ -86,9 +86,9 @@ sudo systemctl restart sshd
 
 wget https://github.com/Meltingplot/BigPrint/archive/refs/heads/duet-3.5.3-sbc.zip
 unzip duet-3.5.3-sbc.zip
-mv BigPrint-duet-3.5.3-sbc/duet-config/filaments/* /opt/dsf/sd/filaments/
-mv BigPrint-duet-3.5.3-sbc/duet-config/macros/* /opt/dsf/sd/macros/
-mv BigPrint-duet-3.5.3-sbc/duet-config/sys/* /opt/dsf/sd/sys/
+mv -f BigPrint-duet-3.5.3-sbc/duet-config/filaments/* /opt/dsf/sd/filaments/
+mv -f BigPrint-duet-3.5.3-sbc/duet-config/macros/* /opt/dsf/sd/macros/
+mv -f BigPrint-duet-3.5.3-sbc/duet-config/sys/* /opt/dsf/sd/sys/
 
 # Change ownership to the dsf user
 sudo chown -R dsf:dsf /opt/dsf/sd/
